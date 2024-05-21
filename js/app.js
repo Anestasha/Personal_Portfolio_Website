533bd290a9b3e680ee9102305b45a33e0f624db4
Anestasha <anna.mcs.2002@gmail.com>
2024-05-21 12:53:43 -0700

    Updated image switching function
    Updated the product image gallery function to handle styling for poster and math pumpkin blocks.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,74 @@
 $(document).foundation()
 
 $('.sim-thumb').on('click', function() {
-    let thmbSource = $(this).data('image');
-    let mainSource = $('#main-product-image').find('img').attr('src');
-
-    let thmbCap = $(this).data('caption');
-    let mainCap = $('#main-product-image').find('figcaption').html();
-
-    // Function to format caption
-    function formatCaption(caption) {
-        // Extract date from caption
-        let match = caption.match(/Created (\w+ \d{4})/);
-        if (match) {
-            let date = match[1];
-            return `Created in <span class="date">${date}</span>`;
+    //'Special Interests' page, theatre poster captions
+    function formatPosterCaption(caption){
+        let dateMatch = caption.match(/Created (\w+ \d{4})/);
+        if(dateMatch){
+            let date = dateMatch[1];
+            return `Created <span class="date">${date}</span>`;
         }
         return caption;
     }
 
-    let formattedThmbCap = formatCaption(thmbCap);
-    let formattedMainCap = formatCaption(mainCap);
+    //'Featured Projects' page, math pumpkin caption
+    function formatPumpkinCaption(caption){
+        let detailMatch = caption.split(" (");
+        console.log(detailMatch[1]);
+        if(detailMatch[1]){
+            let main = detailMatch[0];
+            let details = detailMatch[1];
+            console.log("main: " + main);
+            console.log("details: " + details);
+            return `${main} <br><span class="description">${details}</span>`;
+        }
+        return caption;
+    }
+    
+    //Get the image source for the main and thumbnail images
+    let thumbSource = $(this).data('image');
+    let mainSource = $('#main-product-image').find('img').attr('src');
 
-    $('#main-product-image').find('figure').find('img').attr('src', thmbSource);
+    //Get the image alt tags for the main and thumbnail images
+    let thumbAlt = $(this).find('img').attr('alt');
+    let mainAlt = $('#main-product-image').find('img').attr('alt');
+    console.log("thumb alt = " + thumbAlt);
+    console.log("main alt = " + mainAlt);
+
+    //Get the captions for the main and thumbnail images
+    let thumbCap = $(this).data('caption');
+    let mainCap = $('#main-product-image').find('figcaption').html();
+
+    let formattedThumbCap;
+    let formattedMainCap;
+
+    //Format captions if necessary
+    if($(this).data('type') == 'pumpkin'){
+        formattedThumbCap = formatPumpkinCaption(thumbCap);
+        formattedMainCap = formatPumpkinCaption(mainCap);
+    }
+
+    else if($(this).data('type') == 'poster'){
+        console.log($(this).data('type'));
+
+        formattedThumbCap = formatPosterCaption(thumbCap);
+        formattedMainCap = formatPosterCaption(mainCap);
+    }
+
+    else{
+        formattedThumbCap = thumbCap;
+        formattedMainCap = mainCap; 
+    }
+
+    //Update the main figure to contain the image source, caption and alt tags
+    $('#main-product-image').find('figure').find('img').attr('src', thumbSource);
+    $('#main-product-image').find('figure').find('img').attr('alt', thumbAlt);
+    $('#main-product-image').find('figcaption').html(formattedThumbCap);
+
+
+    //Update the thumbnail li to contain the new image source, caption and alt tags
     $(this).find('img').attr('src', mainSource);
+    $(this).find('img').attr('alt', mainAlt);
     $(this).data('image', mainSource);
-
-    $('#main-product-image').find('figcaption').html(formattedThmbCap);
     $(this).data('caption', formattedMainCap);
-  })
+})
